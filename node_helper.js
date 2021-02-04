469ec03dd4ad9c01104de98261aad00ba90dc0d9
Brian Dorton <brian.dorton@ibm.com>
2021-02-04 09:46:28 -0600

    move api call to local server to handle days without menus

--- node_helper.js
+++ node_helper.js
@@ -11,40 +11,45 @@ var moment = require("moment");
 var _ = require("lodash");
 
 module.exports = NodeHelper.create({
-	socketNotificationReceived: function(notification, payload) {
-		switch (notification) {
-		case "SET_CONFIG":
-			this.config = payload;
-			this.sendSocketNotification("CONFIG_SET");
-			break;
+  socketNotificationReceived: function (notification, payload) {
+    switch (notification) {
+      case "SET_CONFIG":
+        this.config = payload;
+        this.sendSocketNotification("CONFIG_SET");
+        break;
 
-		case "FETCH_DATA":
-			if (this.config.schoolId) {
-				this.fetchData();
-			}
-			break;
-		}
-	},
+      case "FETCH_DATA":
+        if (this.config.schoolId) {
+          this.fetchData();
+        }
+        break;
+    }
+  },
 
-	fetchData: function() {
-		let date = this.config.monday || moment()
-			.day("Monday")
-			.format("L");
-		let mealType = _.capitalize(this.config.mealType);
+  fetchData: function () {
+    let date = this.config.monday || moment().day("Monday").format("L");
+    let mealType = _.capitalize(this.config.mealType);
 		let url = `https://webapis.schoolcafe.com/api/CalendarView/GetWeeklyMenuitems?SchoolId=${this.config.schoolId}&ServingDate=${date}&ServingLine=Line%201&MealType=${mealType}`;
+    let localUrl = "https://admin.dorton.dev/api/lunch";
 
-		request(
-			{
-				url: url,
-				method: "GET"
-			},
-			(error, response, body) => {
-				if (error) {
-					this.sendSocketNotification("NETWORK_ERROR", error);
-				} else {
-					this.sendSocketNotification("DATA_AVAILABLE", response);
-				}
-			}
-		);
-	}
+    let params = {
+      date: date,
+      mealType: mealType,
+      schoolId: this.config.schoolId
+    };
+    request(
+      {
+				url: localUrl,
+				qs: params,
+        method: "GET"
+      },
+      (error, response, body) => {
+        if (error) {
+          this.sendSocketNotification("NETWORK_ERROR", error);
+        } else {
+          this.sendSocketNotification("DATA_AVAILABLE", response);
+        }
+      }
+    );
+  }
 });
